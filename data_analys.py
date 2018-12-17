bfbb25f6d01ee3721a40e4e18c977a8e1236410f
Forevka69 <starfield@i.ua>
2018-12-17 22:49:43 +0200

    added style

--- data_analys.py
+++ data_analys.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np; np.random.seed(1)
 from get_data import get_data
 
-data = get_data()#json.loads(open("data.json").read())['RECORDS'];
+data = get_data(limit = 500)#json.loads(open("data.json").read())['RECORDS'];
 print('loaded')
 x = [datetime.datetime.strptime(i[1], "%Y.%m.%d %H:%M:%S") for i in data]
 y = [n for n, i in enumerate(data)]
@@ -15,8 +15,11 @@ names = np.array([str(i[0]) for i in data])
 norm = plt.Normalize(1,4)
 cmap = plt.cm.RdYlGn
 
+
+
 fig,ax = plt.subplots()
 line, = plt.plot(x,y, marker="o")
+plt.style.use("seaborn-darkgrid")
 
 annot = ax.annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points",
                     bbox=dict(boxstyle="round", fc="w"),
@@ -46,6 +49,7 @@ def hover(event):
                 fig.canvas.draw_idle()
 
 fig.canvas.mpl_connect("motion_notify_event", hover)
-
+plt.xlabel("Time")
+plt.ylabel("Count")
 plt.show()
 
